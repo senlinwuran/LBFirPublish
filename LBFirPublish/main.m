493f96fedf34562151fc60ed34ad1ed5ffdae30d
senlinwuran <609815107@qq.com>
2016-07-18 10:38:59 +0800

    Initial Commit

---
+++ LBFirPublish/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LBFirPublish
+//
+//  Created by LiuBo on 16/7/18.
+//  Copyright © 2016年 com.xuerersi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
